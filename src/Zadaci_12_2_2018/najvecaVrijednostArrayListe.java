7c928279676f9226846460a63832c6f95602a3ef
Syber55 <sajib_causevic@hotmail.com>
2018-02-12 20:14:05 +0100

    Uradjena najveca vrijednost liste

--- src/Zadaci_12_2_2018/najvecaVrijednostArrayListe.java
+++ src/Zadaci_12_2_2018/najvecaVrijednostArrayListe.java
@@ -1,10 +1,29 @@
 package Zadaci_12_2_2018;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class najvecaVrijednostArrayListe {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner in = new Scanner(System.in);
+		ArrayList<Integer> niz = new ArrayList<>();
+		System.out.println("Unesi 5 clanova u niz");
+		for (int i=0; i<5; i++) {
+			int broj = in.nextInt();
+			niz.add(broj);
+		}
+		in.close();
+		System.out.println(max(niz));
 	}
-
+	
+	public static Integer max (ArrayList<Integer> list) {
+		Integer najveci = 0;
+		for (int i=0 ;i<list.size(); i++) {
+			if (najveci < list.get(i)) {
+				najveci = list.get(i);
+			}
+		}
+		
+		return najveci;
+	}
 }
